ee03417c484c55e4d801a4f98cec42a987523a27
cuong <cuong.hoangduc@cxview.ai>
2022-11-30 03:57:03 +0000

    update new schema queue keyword

--- ImportContent.py
+++ ImportContent.py
@@ -365,7 +365,7 @@ def import_content(content, keyword, anchor_text, base_url):
     # find keyword and replace anchor text for test
     print(f"{keyword} ---- {anchor_text}")
     anchor_link = f"""<a href='{base_url}'>{anchor_text}</a>"""
-
+    print(f"re.match(str(keyword), content, re.IGNORECASE): {re.match(str(keyword), content, re.IGNORECASE)}")
     pattern = re.compile(str(keyword), re.IGNORECASE)
     content["content"] = pattern.sub(anchor_link, content["content"], 1)
     # content["content"] = content.get("content").replace(str(keyword["keyword"]), anchor_link, 1)
