aeea0ccdf850710da0612caaa7ae7bedf7893694
cuong <cuong.hoangduc@cxview.ai>
2022-12-26 15:51:22 +0000

    update data spin

--- google_colab.py
+++ google_colab.py
@@ -155,7 +155,7 @@ def ColabSimple():
 
                                 # if len(article.text.split(" ")) >= 300 and (
                                 #         "content=\"vi_" in article.html or "lang=\"vi\"" in article.html):
-                                if len(article.text.split(" ")) >= 300:
+                                if len(article.text.split(" ")) >= 200:
                                     try:
                                         print("get content")
                                         done = get_contents(article, keyword_object[0])
