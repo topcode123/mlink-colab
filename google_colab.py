a36cda553dbecad3b2b8598eaf285d0fcdd04534
cuong <cuong.hoangduc@cxview.ai>
2022-12-28 07:36:01 +0000

    update spin content

--- google_colab.py
+++ google_colab.py
@@ -99,7 +99,7 @@ def ColabSimple():
                                 continue
 
                             keyword_object = [{
-                                "link": "web",
+                                "link": web,
                                 "web_info": keyword["web_info"],
                                 "keyword": keyword["keyword"],
                                 "anchortext": keyword["anchortext"],
