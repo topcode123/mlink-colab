7db14b334c47908ec9ed29fe6c10f0d46806ded1
cuong <cuong.hoangduc@cxview.ai>
2022-12-28 07:36:25 +0000

    update spin content

--- google_colab.py
+++ google_colab.py
@@ -85,12 +85,12 @@ def ColabSimple():
                                 "--------------------------------------------------------------------------------------------")
                             web = web.split("#")[0]
                             total_web = total_web + 1
-                            # if client1.urldone[str(keyword["web_info"]["_id"])].count_documents({"link": web}) > 0:
-                            #     continue
-                            #
-                            # print("check mlink url done")
-                            # if mlink_url_done.count_documents({"link": web}) > 0:
-                            #     continue
+                            if client1.urldone[str(keyword["web_info"]["_id"])].count_documents({"link": web}) > 0:
+                                continue
+                            
+                            print("check mlink url done")
+                            if mlink_url_done.count_documents({"link": web}) > 0:
+                                continue
 
                             domain = urlparse(web).netloc
                             print(f"domain: {domain}")
